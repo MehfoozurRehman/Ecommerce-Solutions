d58f9f3371d0a385ca6b201aeb3fa4f6fd939356
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-11-18 09:32:27 +0500

    deps updated and pnpm applied

--- store/tailwind.config.js
+++ store/tailwind.config.js
@@ -1,3 +1,7 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
@@ -8,4 +12,4 @@ export default {
       extend: {},
     },
     plugins: [],
-  }
+  };
